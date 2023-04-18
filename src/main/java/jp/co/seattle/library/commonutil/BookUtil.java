9ca5fb77a5c8e41a227c8f90b69d857b4fbaeecd
aripi <aripi@192.168.130.218>
2023-04-18 14:17:41 +0900

    task4修正

--- src/main/java/jp/co/seattle/library/commonutil/BookUtil.java
+++ src/main/java/jp/co/seattle/library/commonutil/BookUtil.java
@@ -3,6 +3,7 @@ package jp.co.seattle.library.commonutil;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -58,8 +59,13 @@ public class BookUtil {
 			DateFormat formatter = new SimpleDateFormat("yyyyMMdd");
 			formatter.setLenient(false); // ←これで厳密にチェックしてくれるようになる
 			//TODO　取得した日付の形式が正しければtrue（タスク４）
-			formatter.parse(publishDate);
-			return true;
+			Date date = (Date) formatter.parse(publishDate);
+			String strdate = formatter.format(date);
+			if (publishDate.equals(strdate)) {
+				return true;
+			} else {
+				return false;
+			}
 		} catch (Exception p) {
 			p.printStackTrace();
 			return false;
@@ -74,18 +80,15 @@ public class BookUtil {
 	 */
 	private static boolean isValidIsbn(String isbn) {
 		//TODO　ISBNが半角数字で10文字か13文字であればtrue（タスク４）
-		if (!isbn.isEmpty()) {
-			if (isbn.matches("[0-9]+")) {
-				if (isbn.length() == 10 | isbn.length() == 13) {
-					return true;
-				}
+		if (!StringUtils.isEmpty(isbn)) {
+			if (isbn.matches("^[0-9]+") && (isbn.length() == 10 || isbn.length() == 13)) {
+				return true;
 			} else {
 				return false;
 			}
 		} else {
 			return true;
 		}
-		return true;
 	}
 
 	/**
